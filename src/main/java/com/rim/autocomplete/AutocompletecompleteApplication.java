5d84d668a34eb6a2dbb0c943ae3776bc5ef898e4
Sadot Hernandez <zadot911218@gmail.com>
2018-09-29 18:49:11 -0500

    Initial commit

---
+++ src/main/java/com/rim/autocomplete/AutocompletecompleteApplication.java
@@ -0,0 +1,12 @@
+package com.rim.autocomplete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutocompletecompleteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutocompletecompleteApplication.class, args);
+	}
+}
